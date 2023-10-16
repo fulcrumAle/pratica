a172ec61baf3b06757b17ceee18d29ee1a296679
fulcrumAle <134450730+fulcrumAle@users.noreply.github.com>
2023-10-16 10:31:07 -0400

    Se esta subiendo esta wea

---
+++ C#/practicadetaller/Models/Materia.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace practicadetaller.Models
+{
+    public class Materia
+    {
+        [Key]
+        public int idMateria { get; set; }
+        public string Nombre { get; set; }
+        public int idDocente { get; set; }
+
+    }
+}
